85cfa9092b42b20db773d0276b1f3c403e35c278
Arthur Hovhannisyan <arthurho@adobe.com>
2026-06-03 12:24:51 +0400

    feat: initialize Maven project with Spring Boot 3.3.x skeleton and SQLite + PostgreSQL support

---
+++ src/main/java/com/worldcup/prediction/WorldCupPredictionApplication.java
@@ -0,0 +1,14 @@
+package com.worldcup.prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WorldCupPredictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldCupPredictionApplication.class, args);
+    }
+}
