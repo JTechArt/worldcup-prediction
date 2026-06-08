c5bbe04bf3b45c2810eba8dc95554a0af577b63d
Arthur <hovhannisyan.arthur@gmail.com>
2026-06-08 08:41:34 +0400

    feat: set JVM default timezone from app.timezone property

--- src/main/java/com/worldcup/prediction/WorldCupPredictionApplication.java
+++ src/main/java/com/worldcup/prediction/WorldCupPredictionApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class WorldCupPredictionApplication {
 
     public static void main(String[] args) {
+        String tz = System.getenv().getOrDefault("APP_TIMEZONE", "Asia/Yerevan");
+        TimeZone.setDefault(TimeZone.getTimeZone(tz));
         SpringApplication.run(WorldCupPredictionApplication.class, args);
     }
 }
